5c0369f831c2e3e3afbc4b286212c62227bd6d68
avijjana <avijjana@DIN78000374.corp.capgemini.com>
2019-07-04 12:09:56 +0530

    this is my firstgit hub

---
+++ sample1/src/main/java/sample1/Mainclass.java
@@ -0,0 +1,8 @@
+package sample1;
+
+public class Mainclass {
+public static void display()
+{
+	System.out.println("hello the world" );
+}
+}
---
+++ sample1/src/main/java/sample1/Mainclass1.java
@@ -0,0 +1,10 @@
+package sample1;
+
+public class Mainclass1 {
+public static void main(String args [])
+{
+	Mainclass obj=new Mainclass();
+	obj.display();
+}
+}
+
